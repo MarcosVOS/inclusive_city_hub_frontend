b7bd35c80422cc56ccfc9e1b2fb78fc34a89bf21
MarcosVOS <marcos.oliveiradev@gmail.com>
2025-10-12 14:20:54 -0300

    fix: tsconfig.json

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Button from "@/components/ui/Button";
-import Input from "@/components/ui/input";
+import Input from "@/components/ui/Input";
 import React from "react";
 import Link from "next/link";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Button from "@/components/ui/Button";
-import Input from "@/components/ui/input";
+import Input from "@/components/ui/Input";
 import { ArrowRightIcon } from "@phosphor-icons/react";
 import React from "react";
 
